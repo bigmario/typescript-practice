efa903b21c21f6effa061a715f8fdae13add4bc1
Mario Castro <mariocastro.pva@gmail.com>
2022-06-06 13:18:31 -0400

    number types

---
+++ src/03-typing.ts
@@ -0,0 +1,10 @@
+let myProductName =  'Product 1';
+let myProducPrice =  123;
+
+myProductName = 'change';
+myProductName.toLowerCase();
+
+myProducPrice.toFixed();
+
+const myProductStock = 1000;
+const myProductName2 =  'Product 2';
