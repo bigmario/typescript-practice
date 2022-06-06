a49bd742ced5b2f5d2d79e30e0198980cbbb4ee2
Mario Castro <mariocastro.pva@gmail.com>
2022-06-06 13:56:28 -0400

    booleans

---
+++ src/05-booleans.ts
@@ -0,0 +1,18 @@
+(()=>{
+  let isEnabled = true;
+
+  isEnabled='as';
+  isEnabled=1212;
+  isEnabled=false;
+
+  let isNew: boolean = false;
+  console.log('isNew', isNew);
+  isNew = true;
+  console.log('isNew', isNew);
+
+  const random = Math.random();
+  console.log('random', random);
+  isNew = random >= 0.5 ? true : false
+  console.log('isNew', isNew);
+
+})()
