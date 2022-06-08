288d96acd279a1b02b23b6642a55887d952c3893
Mario Castro <mariocastro.pva@gmail.com>
2022-06-08 15:18:25 -0400

    functions return

---
+++ src/13-return-functions.ts
@@ -0,0 +1,19 @@
+(()=>{
+  const calcTotal = (prices: number[]): string => {
+    let total = 0;
+    prices.forEach((item) => {
+      total += item;
+    });
+    return total.toString();
+  }
+
+  // const rta = calcTotal([1,2,3,4,5,6]);
+  // console.log(rta);
+
+  const printTotal = (prices: number[]):void => {
+    const rta = calcTotal(prices);
+    console.log(`el total es ${rta}`);
+  }
+
+  printTotal([1,2,3,4,5,6]);
+})();
